aecc4da4f6f556fde0431e00b8fee165c0178fa1
dominikhaid <info@dominikhaid.de>
2020-09-25 03:05:49 +0200

    Projektdateien hinzufügen.

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Book> Book {get; set;}
+
+        
+    }
+
+}
